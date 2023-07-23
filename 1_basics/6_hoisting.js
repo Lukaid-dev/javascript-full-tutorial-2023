a5bace9046ea15c2da85ae43f32be84368d8c08d
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-07-23 22:38:50 +0900

    1-8

--- 1_basics/6_hoisting.js
+++ 1_basics/6_hoisting.js
@@ -1,23 +1,29 @@
 /**
  * Hoisting
  */
-console.log('Hello');
-console.log('World');
-console.log('----------');
+// console.log("Hello");
+// console.log("World");
+// console.log("----------");
 
 // console.log(name);
-// var name = '코드팩토리';
+// var name = "코드팩토리";
 // console.log(name);
 
 /**
  * Hoisting은 무엇인가?
- * 
+ *
  * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
  */
 // var name;
 // console.log(name);
-// name = '코드팩토리';
+// name = "코드팩토리";
 // console.log(name);
 
+// var는 hoisting이 발생한다. 그렇다면 let과 const는 hoisting이 발생하지 않을까?
+// let과 const 또한 hoisting이 발생한다. 다만, let과 const는 var과 다르게, ReferenceError: Cannot access 'yuJin' before initialization 에러가 발생한다.
+
 console.log(yuJin);
-const yuJin = '안유진';
+let yuJin = "안유진";
+
+// console.log(yuJin);
+// const yuJin = "안유진";
--- 1_basics/7_operators.js
+++ 1_basics/7_operators.js
@@ -1,12 +1,12 @@
 /**
  * Operators
- * 
+ *
  * 연산자
  */
 
 /**
  * 산술 연산자
- * 
+ *
  * 1) 덧셈
  * 2) 뺄셈
  * 3) 곱셈
@@ -17,10 +17,13 @@ console.log(10 + 10);
 console.log(10 - 10);
 console.log(10 * 10);
 console.log(10 / 10);
+// 나머지 연산자
 console.log(10 % 10);
 console.log(10 % 3);
+// 몫은 Math.floor()를 사용한다.
+console.log(Math.floor(10 / 3));
 
-console.log('-----------');
+console.log("-----------");
 
 console.log(10 * (10 + 10));
 
@@ -34,7 +37,7 @@ console.log(number);
 
 number--;
 console.log(number);
-console.log('---------');
+console.log("---------");
 
 /**
  * 연산자의 위치
@@ -42,12 +45,14 @@ console.log('---------');
 let result = 1;
 console.log(result);
 
+// 이렇게 하면 result에 할당된 후 number가 1 증가한다.
 result = number++;
 console.log(result, number);
 
 result = number--;
 console.log(result, number);
 
+// 이렇게 하면 number가 1 증가하고 result에 할당된다. 근데 이거 쓸일 없음. 오히려 안티패턴이라고 함.
 result = ++number;
 console.log(result, number);
 
@@ -57,7 +62,7 @@ console.log(result, number);
 /**
  * 숫자 타입이 아닌 타입에 +, - 사용하면 어떻게될까?
  */
-let sample = '99';
+let sample = "99";
 
 console.log(+sample);
 console.log(typeof +sample);
@@ -73,11 +78,11 @@ sample = false;
 console.log(+sample);
 console.log(typeof +sample);
 
-sample = '안유진';
+sample = "안유진";
 // NaN -> Not a Number
 console.log(+sample);
 
-sample = '99';
+sample = "99";
 console.log(-sample);
 console.log(typeof -sample);
 
@@ -104,44 +109,48 @@ console.log(number);
 
 /**
  * 비교 연산자
- * 
+ *
  * 1) 값의 비교
- * 2) 값과 타입의 비교
+ * 2) 값과 타입의 비교 - 실무에서는 무조건 이걸 사용한다.
  */
 
+// 값의 비교
 console.log(5 == 5);
-console.log(5 == '5');
-console.log(0 == '');
+console.log(5 == "5");
+console.log(0 == "");
 console.log(true == 1);
 console.log(false == 0);
-console.log(true == '1');
+console.log(true == "1");
 
+// 값과 타입의 비교
 console.log(5 === 5);
-console.log(5 === '5');
-console.log(0 === '');
+console.log(5 === "5");
+console.log(0 === "");
 console.log(true === 1);
 console.log(false === 0);
-console.log(true === '1');
+console.log(true === "1");
 
-console.log('------------');
+console.log("------------");
 
+// 값의 비교
 console.log(5 != 5);
-console.log(5 != '5');
-console.log(0 != '');
+console.log(5 != "5");
+console.log(0 != "");
 console.log(true != 1);
 console.log(false != 0);
-console.log(true != '1');
+console.log(true != "1");
 
-console.log('-------------')
+console.log("-------------");
 
+// 값과 타입의 비교
 console.log(5 !== 5);
-console.log(5 !== '5');
-console.log(0 !== '');
+console.log(5 !== "5");
+console.log(0 !== "");
 console.log(true !== 1);
 console.log(false !== 0);
-console.log(true !== '1');
+console.log(true !== "1");
 
-console.log('------------');
+console.log("------------");
 
 /**
  * 대소 관계 비교 연산자
@@ -154,12 +163,12 @@ console.log(100 >= 1);
 /**
  * 삼항 조건 연산자 (ternary operator)
  */
-console.log(10 > 0 ? '10이 0보다 크다' : '10이 0보다 작다');
+console.log(10 > 0 ? "10이 0보다 크다" : "10이 0보다 작다");
 
-console.log('------------');
+console.log("------------");
 /**
  * 논리 연산자
- * 
+ *
  * 1) && (and)
  * 2) || (or)
  */
@@ -170,14 +179,14 @@ console.log(true && false);
 console.log(false && true);
 console.log(false && false);
 
-console.log('------------');
+console.log("------------");
 
 // ||는 하나만 true여도 true를 반환한다.
 console.log(true || true);
 console.log(true || false);
 console.log(false || true);
 console.log(false || false);
-console.log('------------');
+console.log("------------");
 
 console.log(10 > 1 && 20 > 2);
 console.log(10 < 1 && 20 > 2);
@@ -189,21 +198,21 @@ console.log(10 < 1 || 20 < 2);
 
 /**
  * 단축평가 (short circuit evaluation)
- * 
+ *
  * &&를 사용했을때 좌측이 true면 우측 값 반환
  * &&를 사용했을때 좌측이 false면 좌측 값 반환
  * ||를 사용했을때 좌측이 true면 좌측 값 반환
  * ||를 사용했을때 좌측이 false면 우측 값 반환
  */
-console.log('------------');
+console.log("------------");
 
-console.log(true || '아이브');
-console.log(false || '아이브');
-console.log(false && '아이브');
-console.log(true && '아이브');
+console.log(true || "아이브");
+console.log(false || "아이브");
+console.log(false && "아이브");
+console.log(true && "아이브");
 
-console.log(true && true && '아이브');
-console.log(true && false && '아이브');
+console.log(true && true && "아이브");
+console.log(true && false && "아이브");
 
 /**
  * 지수 연산자
@@ -217,12 +226,14 @@ console.log(10 ** 3);
 let name;
 console.log(name);
 
-name = name ?? '코드팩토리';
+// name이 null이면 ?? 우측의 값을 반환한다.
+name = name ?? "코드팩토리";
 console.log(name);
 
-name = name ?? '아이브';
+name = name ?? "아이브";
 console.log(name);
 
 let name2;
-name2 ??= '코드팩토리';
+// name2가 null이면 ?? 우측의 값을 할당한다.
+name2 ??= "코드팩토리";
 console.log(name2);
